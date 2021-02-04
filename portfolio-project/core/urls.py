8b0c9adcae692fccc464adc03b62a5699288ac3d
prjctakshay <akshaychelembra@gmail.com>
2021-02-04 22:15:29 +0530

    hello.html added <a> tag ling tested

--- portfolio-project/core/urls.py
+++ portfolio-project/core/urls.py
@@ -3,7 +3,7 @@ from core.views import home_test,test
 
 urlpatterns = [
     path('', home_test, name='home'),
-    path('test', test, name='tfest'),
+    path('test', test, name='test'),
     # path('', save, name='save'),
     # path('delete/<int:todo_id>/', delete, name= 'delete'),
 ]
