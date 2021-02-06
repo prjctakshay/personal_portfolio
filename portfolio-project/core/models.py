c2ed8001c2bf695e59d4ea596d25b3e0d77957a7
prjctakshay <akshaychelembra@gmail.com>
2021-02-06 17:39:46 +0530

    index edited models created

--- portfolio-project/core/models.py
+++ portfolio-project/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class AboutMe(models.Model):
+    about_me_bio = models.CharField(max_length=300)
+  
+    
+class Skills(models.Model):
+    skills_text = models.CharField(max_length = 500)
+    # skill_img = models.ImageField()
+
+# its represent What I’m good at? in index page
+class SkillMore(models.Model):
+    skill_name = models.CharField(max_length = 100)
+    skill_discription = models.TextField(max_length= 500)
+    # skill_icon = models.ImageField()
+    
+class MyWork(models.Model):
+    # work_img = models.ImageField()
+    work_title = models.CharField(max_length = 100)
+    short_discription = models.CharField(max_length = 200)
+    git_url = models.URLField()
+    
+    
